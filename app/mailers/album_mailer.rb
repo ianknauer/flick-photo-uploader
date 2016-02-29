5e8ed4bb1cb5b044a757dd0bf1ad72dadcbe70e3
ian knauer <ian.knauer@gmail.com>
2016-02-28 21:05:49 -0800

    added attachment to email

--- app/mailers/album_mailer.rb
+++ app/mailers/album_mailer.rb
@@ -3,5 +3,6 @@ class AlbumMailer < ApplicationMailer
     @customer = album.customer
     @album = album
     mail(to: @customer.email, subject: "Proofs from Images by Wolfgang")
+    attachments['price_list.pdf'] = File.read(Rails.public_path.join("documents/price_list.pdf"))
   end
 end

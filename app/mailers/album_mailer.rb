60cf860340cb5504121c96e7b181edf3b2bd7bd8
ian knauer <ian.knauer@gmail.com>
2016-02-28 21:10:03 -0800

    updated album mailer

--- app/mailers/album_mailer.rb
+++ app/mailers/album_mailer.rb
@@ -2,7 +2,7 @@ class AlbumMailer < ApplicationMailer
   def album_ready_email(album)
     @customer = album.customer
     @album = album
-    mail(to: @customer.email, subject: "Proofs from Images by Wolfgang")
     attachments['price_list.pdf'] = File.read(Rails.public_path.join("documents/price_list.pdf"))
+    mail(to: @customer.email, subject: "Proofs from Images by Wolfgang")
   end
 end
